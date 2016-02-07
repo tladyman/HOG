0b5b4ad573f5e6716844f3470d55df0002b245fa
Olivia Wilson <olivia@rtwilson.com>
2016-02-07 21:05:56 +0000

    Histogram binning started

--- Histogram.py
+++ Histogram.py
@@ -11,12 +11,42 @@ class Histogram:
                     block the shape of the array of blocks is (a,b).
 
     """
-    def __init__(self, block, oBins=8):
+    def __init__(self, gradBlock, magBlock, oBins=9):
         """Constructor from an input array. Array should be 2x2 etc.
 
         Args:
-            block: Block object with blocks and cell arrays within it
+            gradBlock: Block object with blocks and cell arrays within it of the
+                       gradient image
+            magBlock: Block object with blocks and cell arrays within it of the
+                      magnitude image
             oBins: The number of orientation bins to create for the histogram.
-                   default: 8
+                   default: 9 (optimum used in Dalal and Triggs)
         """
-        i = 7
+        
+        # The magnitude image and the gradient image have the same structure
+        # for the array (i.e. each element of a particular position in one array
+        # corresponds to the that position in the other array)
+
+        # Create array of bins using equal bins between 0 and pi (signed)
+        # excludes the end point as that is what will be provided by gradient image
+        bins = np.linspace(-np.pi, np.pi, num=obins - 1, endpoint=False)
+
+        # dim1 and dim2 are the shape of the 
+        dim1 = gradBlock.shape[0]
+        dim2 = gradBlock.shape[0]
+        dim3 = 
+
+        accumulator = np.zeros()
+
+        for i, row in enumerate(gradBlock):
+            for j, col in enumerate(row):
+                # At this point we have cells! and magBlock[i,j] will give us the
+                # corresponding mag block.
+
+                # histogram gives us the option of using a weights array so that
+                # the magnitude provides votes rather than adding 1 each time.
+
+                # Put the corresponding histogram into 
+
+        self.histogram = accumulator
+
